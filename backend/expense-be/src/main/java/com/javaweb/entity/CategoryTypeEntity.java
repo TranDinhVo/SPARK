7d90575eba726b62f28db5d5c5cd71343de98c42
Tran Dinh Vo <trandinhvo1809.com>
2025-04-03 23:37:41 +0700

    chan

--- backend/expense-be/src/main/java/com/javaweb/entity/CategoryTypeEntity.java
+++ backend/expense-be/src/main/java/com/javaweb/entity/CategoryTypeEntity.java
@@ -8,6 +8,8 @@ import com.javaweb.enums.CategoryTypeEnum;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -30,6 +32,7 @@ public class CategoryTypeEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Enumerated(EnumType.STRING)
 	@Column(name="name",nullable = false, length = 100)
 	private CategoryTypeEnum name;
 	
@@ -42,6 +45,14 @@ public class CategoryTypeEntity {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public CategoryTypeEnum getName() {
+		return name;
+	}
+
+	public void setName(CategoryTypeEnum name) {
+		this.name = name;
 	}
 
 	public List<CategoryEntity> getCategories() {
