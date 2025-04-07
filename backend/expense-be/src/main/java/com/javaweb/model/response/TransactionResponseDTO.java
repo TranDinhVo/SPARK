6b8061ef3cca92ab7024f522bd0292b5750579a1
Tran Dinh Vo <trandinhvo1809.com>
2025-04-08 00:03:51 +0700

    transaction response

--- backend/expense-be/src/main/java/com/javaweb/model/response/TransactionResponseDTO.java
+++ backend/expense-be/src/main/java/com/javaweb/model/response/TransactionResponseDTO.java
@@ -4,26 +4,105 @@ package com.javaweb.model.response;
 import java.math.BigDecimal;
 import java.time.Instant;
 
-import com.javaweb.model.TransactionCategoryDTO;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class TransactionResponseDTO {
 	
 	
     private Long id;
-    private BigDecimal amount;
+    private Long userId;
+    private WalletResponseDTO wallet;
+    private Long borrowId;
+//    private TransactionCategoryResponseDTO category;
     private String type;
-    private String description;
+    private String name;
+    private BigDecimal amount;
+    private String description; 
     private Boolean status;
     private Instant createdAt;
-
-    private TransactionCategoryDTO category; 
-    private String paymentMethod; // Chỉ lấy paymentMethod từ WalletEntity
-    private RecurringTransactionResponseDTO recurrence; // Format riêng cho RecurringTransactionEntity
-//    private BorrowingDTO borrowing; // Thêm BorrowingEntity
-//    private GoalDTO goal; // Thêm GoalEntity
+    private RecurringTransactionResponseDTO recurrence;
+    
+	public TransactionResponseDTO(Long id, Long userId, WalletResponseDTO wallet, Long borrowId, String type,
+			String name, BigDecimal amount, String description, Boolean status, Instant createdAt,
+			RecurringTransactionResponseDTO recurrence) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.wallet = wallet;
+		this.borrowId = borrowId;
+		this.type = type;
+		this.name = name;
+		this.amount = amount;
+		this.description = description;
+		this.status = status;
+		this.createdAt = createdAt;
+		this.recurrence = recurrence;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public WalletResponseDTO getWallet() {
+		return wallet;
+	}
+	public void setWallet(WalletResponseDTO wallet) {
+		this.wallet = wallet;
+	}
+	public Long getBorrowId() {
+		return borrowId;
+	}
+	public void setBorrowId(Long borrowId) {
+		this.borrowId = borrowId;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getAmount() {
+		return amount;
+	}
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Boolean getStatus() {
+		return status;
+	}
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Instant createdAt) {
+		this.createdAt = createdAt;
+	}
+	public RecurringTransactionResponseDTO getRecurrence() {
+		return recurrence;
+	}
+	public void setRecurrence(RecurringTransactionResponseDTO recurrence) {
+		this.recurrence = recurrence;
+	} 
+    
+    
+    
 }
