dfda6407eb88ac1247088f44cd5594e18ddd9931
Huyy5725 <huyy5725@gmail.com>
2025-04-13 17:34:44 +0700

    fix category

--- backend/expense-be/src/main/java/com/javaweb/repository/RoleRepository.java
+++ backend/expense-be/src/main/java/com/javaweb/repository/RoleRepository.java
@@ -1,13 +1,13 @@
-
-package com.javaweb.repository;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.javaweb.entity.RoleEntity;
-
-public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
-	Optional<RoleEntity> findByCode(String code);
-	
-}
+//
+//package com.javaweb.repository;
+//
+//import java.util.Optional;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.javaweb.entity.RoleEntity;
+//
+//public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+//	Optional<RoleEntity> findByCode(String code);
+//	
+//}
