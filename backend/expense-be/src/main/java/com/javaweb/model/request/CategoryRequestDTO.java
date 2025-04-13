68b7eade0809fff362729ca7f6b082d40d06ce8b
Huyy5725 <huyy5725@gmail.com>
2025-04-13 17:34:44 +0700

    fix category

--- backend/expense-be/src/main/java/com/javaweb/model/request/CategoryRequestDTO.java
+++ backend/expense-be/src/main/java/com/javaweb/model/request/CategoryRequestDTO.java
@@ -1,51 +1,51 @@
-//package com.javaweb.model.request;
-//
-//public class CategoryRequestDTO {
-//	private Long userId;
-//	private String name;
-//	private String type;
-//	private String iconUrl;
-//	private String color;
-//	public CategoryRequestDTO() {
-//	}
-//	public CategoryRequestDTO(Long userId, String name, String type, String iconUrl, String color) {
-//		super();
-//		this.userId = userId;
-//		this.name = name;
-//		this.type = type;
-//		this.iconUrl = iconUrl;
-//		this.color = color;
-//	}
-//	public Long getUserId() {
-//		return userId;
-//	}
-//	public void setUserId(Long userId) {
-//		this.userId = userId;
-//	}
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	public String getType() {
-//		return type;
-//	}
-//	public void setType(String type) {
-//		this.type = type;
-//	}
-//	public String getIconUrl() {
-//		return iconUrl;
-//	}
-//	public void setIconUrl(String iconUrl) {
-//		this.iconUrl = iconUrl;
-//	}
-//	public String getColor() {
-//		return color;
-//	}
-//	public void setColor(String color) {
-//		this.color = color;
-//	}
-//	
-//	
-//}
+package com.javaweb.model.request;
+
+public class CategoryRequestDTO {
+	private Long userId;
+	private String name;
+	private String type;
+	private String iconUrl;
+	private String color;
+	public CategoryRequestDTO() {
+	}
+	public CategoryRequestDTO(Long userId, String name, String type, String iconUrl, String color) {
+		super();
+		this.userId = userId;
+		this.name = name;
+		this.type = type;
+		this.iconUrl = iconUrl;
+		this.color = color;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getIconUrl() {
+		return iconUrl;
+	}
+	public void setIconUrl(String iconUrl) {
+		this.iconUrl = iconUrl;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+}
