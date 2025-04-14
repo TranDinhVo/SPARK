6d210de4888303820caaa1241414c51d807044ac
Trần Đình Võ <trandinhvo1809@gmail.com>
2025-04-15 00:15:56 +0700

    ..

--- backend/expense-be/src/main/java/com/javaweb/controller/UserController.java
+++ backend/expense-be/src/main/java/com/javaweb/controller/UserController.java
@@ -1,59 +1,59 @@
-//package com.javaweb.controller;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.javaweb.entity.UserEntity;
-//import com.javaweb.model.request.UserRegisterRequestDTO;
-//import com.javaweb.model.response.UserResponseDTO;
-//import com.javaweb.service.UserService;
-//
-//@RestController
-//@RequestMapping("/api/users")
-//public class UserController {
-//
-//	@Autowired
-//    private UserService userService;
-//
-//    @PostMapping("/register")
-//    public ResponseEntity<?> registerUser(@RequestBody UserRegisterRequestDTO user) {
-//        Optional<UserEntity> checkEmail = userService.findByEmail(user.getEmail());
-//        if (checkEmail.isPresent()) {
-//            return ResponseEntity.badRequest().body("Email đã được sử dụng!");
-//        }
-//        Optional<UserEntity> checkUsername = userService.findByUsername(user.getUsername());
-//        if (checkUsername.isPresent()) {
-//            return ResponseEntity.badRequest().body("Tên đăng nhập đã được sử dụng!");
-//        }
-//        UserResponseDTO newUser = userService.registerUser(user);
-//        return ResponseEntity.ok(newUser);
-//    }
-//
-//    @GetMapping
-//    public List<UserResponseDTO> getAllUsers() {
-//        return userService.getAllUsers();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<?> getUserById(@PathVariable Long id) {
-//        return userService.getUserById(id)
-//                .map(ResponseEntity::ok)
-//                .orElse(ResponseEntity.notFound().build());
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
-//        userService.deleteUser(id);
-//        return ResponseEntity.ok("Xóa user thành công!");
-//    }
-//}
+package com.javaweb.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javaweb.entity.UserEntity;
+import com.javaweb.model.request.UserRegisterRequestDTO;
+import com.javaweb.model.response.UserResponseDTO;
+import com.javaweb.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+	@Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody UserRegisterRequestDTO user) {
+        Optional<UserEntity> checkEmail = userService.findByEmail(user.getEmail());
+        if (checkEmail.isPresent()) {
+            return ResponseEntity.badRequest().body("Email đã được sử dụng!");
+        }
+        Optional<UserEntity> checkUsername = userService.findByUsername(user.getUsername());
+        if (checkUsername.isPresent()) {
+            return ResponseEntity.badRequest().body("Tên đăng nhập đã được sử dụng!");
+        }
+        UserResponseDTO newUser = userService.registerUser(user);
+        return ResponseEntity.ok(newUser);
+    }
+
+    @GetMapping
+    public List<UserResponseDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("Xóa user thành công!");
+    }
+}
--- backend/expense-be/src/main/java/com/javaweb/model/request/UserRegisterRequestDTO.java
+++ backend/expense-be/src/main/java/com/javaweb/model/request/UserRegisterRequestDTO.java
@@ -1,70 +1,70 @@
-//package com.javaweb.model.request;
-//
-//import java.util.List;
-//
-//import com.javaweb.entity.RoleEntity;
-//
-//public class UserRegisterRequestDTO {
-//    private String username;
-//    private String fullname;
-//    private String password;
-//    private String email;
-//    private String phone;
-//    private List<RoleEntity> roles;
-//    
-//    
-//    
-//    public UserRegisterRequestDTO() {}
-//    
-//	public UserRegisterRequestDTO(String username, String fullname, String password, String email, String phone,
-//			List<RoleEntity> roles) {
-//		super();
-//		this.username = username;
-//		this.fullname = fullname;
-//		this.password = password;
-//		this.email = email;
-//		this.phone = phone;
-//		this.roles = roles;
-//	}
-//	public List<RoleEntity> getRoles() {
-//		return roles;
-//	}
-//	public void setRoles(List<RoleEntity> roles) {
-//		this.roles = roles;
-//	}
-//	
-//	public String getUsername() {
-//		return username;
-//	}
-//	public void setUsername(String username) {
-//		this.username = username;
-//	}
-//	public String getFullname() {
-//		return fullname;
-//	}
-//	public void setFullname(String fullname) {
-//		this.fullname = fullname;
-//	}
-//	public String getPassword() {
-//		return password;
-//	}
-//	public void setPassword(String password) {
-//		this.password = password;
-//	}
-//	public String getEmail() {
-//		return email;
-//	}
-//	public void setEmail(String email) {
-//		this.email = email;
-//	}
-//	public String getPhone() {
-//		return phone;
-//	}
-//	public void setPhone(String phone) {
-//		this.phone = phone;
-//	}
-//    
-//    
-//    
-//    
-//}
+package com.javaweb.model.request;
+
+import java.util.List;
+
+import com.javaweb.entity.RoleEntity;
+
+public class UserRegisterRequestDTO {
+    private String username;
+    private String fullname;
+    private String password;
+    private String email;
+    private String phone;
+    private List<RoleEntity> roles;
+    
+    
+    
+    public UserRegisterRequestDTO() {}
+    
+	public UserRegisterRequestDTO(String username, String fullname, String password, String email, String phone,
+			List<RoleEntity> roles) {
+		super();
+		this.username = username;
+		this.fullname = fullname;
+		this.password = password;
+		this.email = email;
+		this.phone = phone;
+		this.roles = roles;
+	}
+	public List<RoleEntity> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<RoleEntity> roles) {
+		this.roles = roles;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+    
+    
+    
+    
+}
