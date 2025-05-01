2b7401d9b2d3dff108a1207560d968800f69088c
Trần Đình Võ <trandinhvo1809@gmail.com>
2025-05-01 21:09:14 +0700

    fix delete Transaction

--- backend/expense-be/src/main/java/com/javaweb/controller/TransactionController.java
+++ backend/expense-be/src/main/java/com/javaweb/controller/TransactionController.java
@@ -1,13 +1,26 @@
 package com.javaweb.controller;
-import com.javaweb.model.request.TransactionRequestDTO;
-import com.javaweb.model.response.TransactionResponseDTO;
-import com.javaweb.service.TransactionService;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.time.Instant;
-import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javaweb.model.request.TransactionRequestDTO;
+import com.javaweb.model.response.TransactionResponseDTO;
+import com.javaweb.service.TransactionService;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
@@ -51,9 +64,21 @@ public class TransactionController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTransaction(@PathVariable Long id) {
-        boolean isDeleted = transactionService.deleteTransaction(id);
-        return isDeleted ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<Map<String, Object>> deleteTransaction(@PathVariable Long id) {
+//        boolean isDeleted = transactionService.deleteTransaction(id);
+//        return isDeleted ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    	boolean isDeleted = transactionService.deleteTransaction(id);
+
+    	if (isDeleted) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("message", "Xóa giao dịch thành công");
+            response.put("id", id);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            Map<String, Object> error = new HashMap<>();
+            error.put("message", "Không tìm thấy giao dịch");
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/user/{userId}/date-range")
